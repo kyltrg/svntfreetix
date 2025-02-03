cfdcea0a836f970b0a7c1b798842ec35d6b02749
kyltrg <tarogkyle1@gmail.com>
2025-02-03 23:58:28 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,38 +1,48 @@
-let noClickCount = 0;
-const noTexts = [
-    "Are you sure love?",
-    "Aww love naman, sure??",
-    "DON'T DO THIS TO MEEEE!!!!",
-    "PLEASEEE LOVE LEILAAA!!! HUHUHU"
-];
+let clickCount = 0;
+const yesButton = document.getElementById('yes-button');
+const noButton = document.getElementById('no-button');
+const message = document.getElementById('message');
+const image = document.getElementById('image');
 
-function noClicked() {
-    if (noClickCount < noTexts.length) {
-        let yesButton = document.getElementById("yesBtn");
-        let noButton = document.getElementById("noBtn");
+const imageUrls = ['please_1.jpg', 'please_2.jpg', 'please_3.jpg', 'please_4.jpg', 'please_5.jpg'];
+const finalImage = 'YEYYY.jpg';
 
-        // Increase Yes button size
-        let currentSize = parseInt(window.getComputedStyle(yesButton).fontSize);
-        yesButton.style.fontSize = (currentSize * 1.5) + "px";
-        yesButton.style.padding = "20px 40px";
-
-        // Change No button text
-        noButton.textContent = noTexts[noClickCount];
-        noClickCount++;
-    } else {
-        document.getElementById("yesBtn").style.fontSize = "50px";
-        document.getElementById("yesBtn").style.width = "300px";
-        document.getElementById("yesBtn").style.height = "100px";
-        document.getElementById("yesBtn").textContent = "YES!!!";
-        document.getElementById("noBtn").classList.add("hidden");
+noButton.addEventListener('click', function() {
+    clickCount++;
+    if (clickCount === 1) {
+        // First click of "No"
+        image.src = imageUrls[1];
+        yesButton.classList.add('enlarged');
+        noButton.textContent = "Are you sure love?";
+        noButton.classList.add('modified');
+    } else if (clickCount === 2) {
+        // Second click of "No"
+        image.src = imageUrls[2];
+        yesButton.classList.add('enlarged-x2');
+        noButton.textContent = "Aww love naman, sure??";
+    } else if (clickCount === 3) {
+        // Third click of "No"
+        image.src = imageUrls[3];
+        yesButton.classList.add('enlarged-x3');
+        noButton.textContent = "DON'T DO THIS TO MEEEE!!!!";
+    } else if (clickCount === 4) {
+        // Fourth click of "No"
+        image.src = imageUrls[4];
+        noButton.textContent = "PLEASEEE LOVE LEILAAA!!! HUHUHU";
     }
-}
+    if (clickCount >= 5) {
+        // If clicked 5 times, show final screen
+        yesButton.style.transform = 'scale(10)';
+        message.textContent = "YES!!!";
+        image.src = finalImage;
+        noButton.style.display = 'none';
+    }
+});
 
-function yesClicked() {
-    document.body.innerHTML = `
-        <div class="container">
-            <img src="YEYYY.jpg" alt="YEYYY!" style="width: 300px;">
-            <h1 style="font-family: 'Kissing Season', cursive; color: #d63384;">YEYYY!!!</h1>
-        </div>
-    `;
-}
+yesButton.addEventListener('click', function() {
+    const audio = new Audio('voice-message.mp3');
+    audio.play();
+    setTimeout(() => {
+        alert('YEYYY!!!');
+    }, audio.duration * 1000);
+});
